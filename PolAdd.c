5d5145fc3bc771fedeffa076ebcbd716ae555a36
Adhil Salim <bizadhil@gmail.com>
2022-10-05 08:06:37 +0530

    Initial commit
    +getting terms

---
+++ PolAdd.c
@@ -0,0 +1,67 @@
+/*
+name: PolAdd
+desc: This program performs addition on polynomial expression
+author: Adhil Salim
+date: 05/10/2022 (last modified)
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    int totalPolEqns = 0, totalTerms = 0;
+
+    // getting the number of polynomial equations
+    printf("Enter the number of polynomial equations: ");
+    scanf("%d", &totalPolEqns);
+
+    // for storing the number of terms in each polynomial equation
+    int totalTermsInEachPol[totalPolEqns];
+
+    // getting the number of terms in each polynomial equation
+    for (int i = 0; i < totalPolEqns; i++)
+    {
+        printf("Enter the number of terms in polynomial equation %d: ", i + 1);
+        scanf("%d", &totalTermsInEachPol[i]);
+
+        //
+        totalTerms += totalTermsInEachPol[i];
+    }
+
+    // array that contains coefficient and exponent of each term of all polynomial equations
+    int mainPol[totalTerms][2];
+
+    // getting the coefficient and exponent of each term of all polynomial equations
+    for (int i = 0; i < totalPolEqns; i++)
+    {
+
+        while (totalTermsInEachPol[i] != 0)
+        {
+            // log 1
+            printf("\nLOG:   TOTAL-TERMS-IN-EACH-POL[%d]: %d\n", i, totalTermsInEachPol[i]);
+
+            printf("Enter the coefficient of term %d of polynomial equation %d: ", totalTermsInEachPol[i], i + 1);
+            scanf("%d", &mainPol[i][0]);
+
+            // log 2
+            printf("LOG:   MAIN-POL[%d][0]: %d\n", i, mainPol[i][0]);
+
+            printf("Enter the exponent of term %d of polynomial equation %d: ", totalTermsInEachPol[i], i + 1);
+            scanf("%d", &mainPol[i][1]);
+
+            // log 3
+            printf("LOG:   MAIN-POL[%d][1]: %d", i, mainPol[i][0]);
+            totalTermsInEachPol[i]--;
+        }
+    }
+
+    // printing the array of polynomial equations
+    for (int i = 0; i < totalTerms; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%d ", mainPol[i][j]);
+        }
+        printf("\n");
+    }
+}
