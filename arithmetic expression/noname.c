c09f53300971eeec0ef6acbed76635b0c3f82c1c
Adhil Salim <bizadhil@gmail.com>
2022-10-06 19:47:59 +0530

    Update noname.c

--- arithmetic expression/noname.c
+++ arithmetic expression/noname.c
@@ -3,8 +3,10 @@ AUTH: ADHIL SALIM
 */
 
 #include <stdio.h>
-#include <stdbool.h> //for bool,true,false
+#include <stdbool.h>
 
+// function to check if the character is an operator
+// requires header file <stdbool.h>
 bool isOperator(char c)
 {
     if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
@@ -17,33 +19,74 @@ bool isOperator(char c)
     }
 }
 
+int orderOfOperator(char c)
+{
+    if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '^')
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 void main()
 {
-
+    // setting up variables
     int totalChars = 0, stack_TOP = -1;
 
+    // getting input
     printf("Entr total number of characters in the arithmetic expression: ");
     scanf("%d", &totalChars);
 
-    char expression[totalChars];
-    char stack[totalChars];
+    char expression[totalChars]; // array to store the expression
+    char stack[totalChars];      // stack for operators
 
+    stack_TOP++;
+    stack[stack_TOP] = '('; // pushing '(' to the stack to mark the end of the expression
+
+    // getting expression
     printf("Enter the arithmetic expression: ");
     scanf("%s", expression);
 
-    printf("The expression is: %s", expression);
+    // printf("The expression is: %s\n", expression);
 
-    printf("\n");
-
+    // looping through the expression
     for (int i = 0; i < totalChars; i++)
     {
         if (!isOperator(expression[i]))
         {
-            // printf("not an operator\n");
+            printf("%c", expression[i]);
         }
         else
         {
-            // printf("operator\n");
+            // if the order of current op is greater than the order of op in stack then push to stack
+            if (orderOfOperator(expression[i]) > orderOfOperator(stack[stack_TOP]))
+            {
+
+                stack_TOP++;
+                stack[stack_TOP] = expression[i];
+            }
+            else
+            {
+                while (orderOfOperator(expression[i]) <= orderOfOperator(stack[stack_TOP]))
+                {
+                    printf("%c", stack[stack_TOP]);
+                    stack_TOP--;
+                }
+                stack_TOP++;
+                stack[stack_TOP] = expression[i];
+            }
         }
     }
+    // printf("%c", stack[stack_TOP]);
 }
