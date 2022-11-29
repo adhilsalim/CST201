d9e95757ced4e47d881aa3c11076ddf96554e899
Adhil Salim <bizadhil@gmail.com>
2022-11-29 20:39:32 +0530

    Update linkedlist by adhilsalim.c

--- Linked List/linkedlist by adhilsalim.c
+++ Linked List/linkedlist by adhilsalim.c
@@ -18,7 +18,7 @@ void createLL()
 {
     if (linkedListCreated == false)
     {
-        printf("\n\n| Creating a LinkedList |\n\n");
+        printf("\n\n| Creating a Linked List |\n\n");
         head = (struct node *)malloc(sizeof(struct node));
         printf("[CONSOLE]Linked List created");
         linkedListCreated = true;
@@ -26,7 +26,7 @@ void createLL()
     }
     else
     {
-        printf("\n[CONSOLE]A LinkedList is already created.\n");
+        printf("\n[CONSOLE]A Linked List is already created.\n");
     }
 }
 
@@ -35,9 +35,32 @@ void displayLL()
     printf("Hello");
 }
 
+//==========================INSERT LL=========================//
 void insertLL()
 {
-    printf("Hello");
+    int choice;
+    printf("\n\n[INSERT ELEMENT]Selection an operation number from the list: \n");
+    printf("1.Insertion at front of Linked List.\n2.Insertion at end of Linked List.\nInsertion at particular position.\n4.Exit.");
+    printf("Select an operation number: ");
+    scanf("%d", % choice);
+
+    switch (choice)
+    {
+    case 1:
+        insertF();
+        break;
+    case 2:
+        insertE();
+        break;
+    case 3:
+        insertP();
+        break;
+    case 4:
+        insertE();
+        break;
+    default:
+        break;
+    }
 }
 
 void deleteLL()
@@ -52,7 +75,7 @@ void main()
     while (true)
     {
         printf("\n\n[HOME] Select an operation from the list: \n");
-        printf("1.Create LinkedList\n2.Display LinkedList\n3.Insert element to LinkedList\n4.Delete element from LinkedList\n5.Exit");
+        printf("1.Create Linked List\n2.Display Linked List\n3.Insert element to Linked List\n4.Delete element from Linked List\n5.Exit");
         printf("\n\nOperation Number: ");
         scanf("%d", &choice);
 
