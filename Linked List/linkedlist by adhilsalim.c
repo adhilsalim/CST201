28d7db8e2e13d1dada11e8482a6d5cd78687c6be
Adhil Salim <bizadhil@gmail.com>
2022-11-29 20:10:30 +0530

    Update linkedlist by adhilsalim.c

--- Linked List/linkedlist by adhilsalim.c
+++ Linked List/linkedlist by adhilsalim.c
@@ -1,16 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void createLL()
+{
+}
+
+void displayLL()
+{
+}
+
+void insertLL()
+{
+}
+
+void deleteLL()
+{
+}
 
 void main()
 {
+    int choice;
 
-    printf("Select an operation from the list: ");
-    printf("1.Create LinkedList\n
-            2.Display LinkedList\n
-            3.Insert element to LinkedList\n
-            4.Delete element from LinkedList");
-    printf("\nOperation Number: ");
+    printf(" [HOME] Select an operation from the list: \n");
+    printf("1.Create LinkedList\n2.Display LinkedList\n3.Insert element to LinkedList\n4.Delete element from LinkedList\n5.Exit");
+    printf("\n\nOperation Number: ");
+    scanf("%d", &choice);
 
-    // while(true){
-
-    //}
+    while (true)
+    {
+        switch (choice)
+        {
+        case 1:
+            createLL();
+            break;
+        case 2:
+            displayLL();
+        case 3:
+            insertLL();
+            break;
+        case 4:
+            deleteLL();
+        case 5:
+            exit(0);
+        default:
+            printf("\nInvalid operation number.\n");
+            break;
+        }
+    }
 }
