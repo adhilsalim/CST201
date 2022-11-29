2572c6b98149431f894d0759b53b63dfff884d4f
Adhil Salim <bizadhil@gmail.com>
2022-11-29 19:57:10 +0530

    Create linkedlist by adhilsalim.c

---
+++ Linked List/linkedlist by adhilsalim.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+
+    printf("Select an operation from the list: ");
+    printf("1.Create LinkedList\n
+            2.Display LinkedList\n
+            3.Insert element to LinkedList\n
+            4.Delete element from LinkedList");
+    printf("\nOperation Number: ");
+
+    // while(true){
+
+    //}
+}
