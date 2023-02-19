29806d26c57be84f41c92a9df7afc9209c535349
Adhil Salim <bizadhil@gmail.com>
2023-02-19 20:39:55 +0530

    Update graph.c

--- Graph/graph.c
+++ Graph/graph.c
@@ -7,10 +7,7 @@ void main()
     printf("No of vertices: ");
     scanf("%d", &TOTAL_VERTICES);
 
-    int row = TOTAL_VERTICES + 1;
-    int col = row;
-
-    int GRAPH_MATRIX[row][col];
+    int GRAPH_MATRIX[TOTAL_VERTICES][TOTAL_VERTICES];
 
     for (int i = 0; i <= TOTAL_VERTICES; i++)
     {
@@ -18,14 +15,6 @@ void main()
         {
             GRAPH_MATRIX[i][j] = 0;
         }
-    }
-
-    printf("Enter vertices in order: \n");
-
-    for (int i = 1; i <= TOTAL_VERTICES; i++)
-    {
-        scanf("%d", &GRAPH_MATRIX[0][i]);
-        GRAPH_MATRIX[i][0] = GRAPH_MATRIX[0][i];
     }
 
     for (int i = 0; i <= TOTAL_VERTICES; i++)
