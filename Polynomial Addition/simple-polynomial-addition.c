f84eec5def370bbf5dd84f710a7a002a1deb5360
Adhil Salim <bizadhil@gmail.com>
2023-02-20 15:34:21 +0530

    Update simple-polynomial-addition.c

--- Polynomial Addition/simple-polynomial-addition.c
+++ Polynomial Addition/simple-polynomial-addition.c
@@ -26,4 +26,36 @@ void main()
         POLYNOMIAL_ONE[0][i] = coefficient;
         POLYNOMIAL_ONE[1][i] = exponent;
     }
+
+    printf("\nENTER POLYNOMIAL TWO: \n");
+    for (int i = 0; i < TOTAL_TERMS_TWO; i++)
+    {
+        printf("coefficient of term %d: ", i + 1);
+        scanf("%d", &coefficient);
+        printf("exponent of term %d: ", i + 1);
+        scanf("%d", &exponent);
+
+        POLYNOMIAL_TWO[0][i] = coefficient;
+        POLYNOMIAL_TWO[1][i] = exponent;
+    }
+
+    printf("\nPOLYNOMIAL TWO: ");
+    for (int i = 0; i < TOTAL_TERMS_ONE; i++)
+    {
+        printf("%dX^%d", POLYNOMIAL_ONE[0][i], POLYNOMIAL_ONE[1][i]);
+        if (i != TOTAL_TERMS_ONE - 1)
+        {
+            printf(" + ");
+        }
+    }
+
+    printf("\nPOLYNOMIAL TWO: ");
+    for (int i = 0; i < TOTAL_TERMS_TWO; i++)
+    {
+        printf("%dX^%d", POLYNOMIAL_TWO[0][i], POLYNOMIAL_TWO[1][i]);
+        if (i != TOTAL_TERMS_TWO - 1)
+        {
+            printf(" + ");
+        }
+    }
 }
